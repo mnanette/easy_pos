c8d4f450d1cf6b89cbc8404202cfcaeda2cf120f
mnanette <mnanette@hotmail.com>
2024-06-26 12:56:43 +0300

    Update client.dart
    Omit email, and address

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -2,15 +2,13 @@ class ClientData {
   int? id;
   String? name;
   String? phone;
-  String? email;
-  String? address;
+  
 
   ClientData.fromJson(Map<String, dynamic> data) {
     id = data["id"];
     name = data["name"];
     phone = data["description"];
-    email = data["email"];
-    address = data["address"];
+   
 
     //@override
     //String? toString() => name; // Return the name as a string
@@ -21,8 +19,7 @@ class ClientData {
       "id": id,
       "name": name,
       "Phone": phone,
-      "Email": email,
-      "Address": address
+     
     };
   }
 }
